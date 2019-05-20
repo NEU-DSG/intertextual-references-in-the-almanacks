93ce04437bb136a0822f172f7c17f01608b07d2b
Ashley M. Clark <amclark42@gmail.com>
2019-05-20 10:55:35 -0400

    Add labels for the genres represented in the network graph

--- data.js
+++ data.js
@@ -34,311 +34,154 @@ var name = "persName";
 document.getElementById("element").innerHTML = "Person Names";
 var redraws = 0;
 
-// buttons
-d3.select("#btn-persName")
-  .classed("button-clicked", true)
-  .on("click",function(){
-    d3.selectAll(".btn-primary").classed("button-clicked", false);
-    d3.select(this).classed("button-clicked", true);
-    document.getElementById("element").innerHTML = "Person Names";
-    redraws++;
-    name = "persName";
-    drawNetwork(name);
-  });
-
-d3.select("#btn-orgName")
-  .on("click",function(){
-    d3.selectAll(".btn-primary").classed("button-clicked", false);
-    d3.select(this).classed("button-clicked", true);
-    document.getElementById("element").innerHTML = "Organization Names";
-    redraws++;
-    name = "orgName";
-    drawNetwork(name);
-  });
-
-d3.select("#btn-placeName")
-  .on("click",function(){
-    d3.selectAll(".btn-primary").classed("button-clicked", false);
-    d3.select(this).classed("button-clicked", true);
-    document.getElementById("element").innerHTML = "Place Names";
-    redraws++;
-    name = "placeName";
-    drawNetwork(name);
-  });
-
 var meta;
 
-drawNetwork(name);
+drawNetwork();
 
-function drawNetwork(name){
+function drawNetwork(){
 
-  var queue = d3.queue();
-  if (!meta) {
-    queue.defer(d3.csv, "wwo-metadata_2017-12-06.csv")
-  }
+  d3.json('intertextual-gestures-mme.json').then( function(data) {
+    meta = data;
+    console.log(meta);
+    
+    // Create Map of distinct genre values with the bibliography IDs and MME gestures they map to.
+    var genreGrp = new Map(),
+        qTypes = new Map();
+    meta['bibliography'].forEach( function(entry) {
+      var id = entry['id'],
+          mainGenre = entry['genreBroad'],
+          gestures = meta['gestures'],
+          grpInner = genreGrp.get(mainGenre) || new Map();
+      gestures = gestures.filter(gesture => gesture['sources'].some(src => src.id === id));
+      grpInner.set(id, gestures);
+      genreGrp.set(mainGenre, grpInner);
+    });
+    meta['genres'] = genreGrp;
+    console.log(meta);
+    
+    /*var colorScaleGenres = d3.scaleOrdinal()
+        .domain(['philosophy', 'religious-writings', 'literature', 'life-writings', 'nonfiction', 'reviews']);*/
+    
+    dispatch.call("dataLoaded", null, meta);
+    
+    
+    function dataTransform(meta, elem){
 
-  queue.defer(d3.csv, "wwo_element_" + name + ".csv")
-    .await(function(error, m, elem) {
-      if (error) throw error;
+      // create array of distinct in-text element values
+      var elemDist = d3.nest()
+        .key(function(d){return d.element})
+        .rollup(function(d){return d.length})
+        .entries(elem);
 
-      if (!meta) {
-        meta = m;
-        meta.forEach(function(i){
-
-              if(i.pubDate == "1558-01-23"){
-                i.pubDate = "1558";
-              }
-              if(i.pubDate == "1601-11-30"){
-                i.pubDate = "1601";
-              }
-              if(i.pubDate == "1643-01-28"){
-                i.pubDate = "1643";
-              }
-              if(i.pubDate == "1760-1761"){
-                i.pubDate = "1760";
-              }
-              i.pubDate = new Date(+i.pubDate,0,1);
-
-              if(i.mainGenre == "G.drama" ||
-                i.mainGenre == "G.drama.prose" ||
-                i.mainGenre == "G.drama.verse" ||
-                i.mainGenre == "G.drama.mixed"){
-                  i.genre = "Drama";
-                  if(i.mainGenre == "G.drama"){ i.mainGenre = "Drama"; }
-                  if(i.mainGenre == "G.drama.prose"){ i.mainGenre = "Drama: Prose"; }
-                  if(i.mainGenre == "G.drama.verse"){ i.mainGenre = "Drama: Verse"; }
-                  if(i.mainGenre == "G.drama.mixed"){ i.mainGenre = "Drama: Mixed"; }
-              }
-              else if(i.mainGenre == "G.non-fiction" ||
-                i.mainGenre == "G.non-fiction.other" ||
-                i.mainGenre == "G.non-fiction.essay" ||
-                i.mainGenre == "G.non-fiction.letter"){
-                  i.genre = "Non-fiction";
-                  if(i.mainGenre == "G.non-fiction"){ i.mainGenre = "Non-fiction"; }
-                  if(i.mainGenre == "G.non-fiction.other"){ i.mainGenre = "Non-fiction: Other"; }
-                  if(i.mainGenre == "G.non-fiction.essay"){ i.mainGenre = "Non-fiction: Essay"; }
-                  if(i.mainGenre == "G.non-fiction.letter"){ i.mainGenre = "Non-fiction: Letter"; }
-              }
-              else if(i.mainGenre == "G.verse" ||
-                i.mainGenre == "G.verse.lyric"||
-                i.mainGenre == "G.verse.narrative" ||
-                i.mainGenre == "G.verse.other"){
-                  i.genre = "Verse";
-                  if(i.mainGenre == "G.verse"){ i.mainGenre = "Verse"; }
-                  if(i.mainGenre == "G.verse.lyric"){ i.mainGenre = "Verse: Lyric"; }
-                  if(i.mainGenre == "G.verse.narrative"){ i.mainGenre = "Verse: Narrative"; }
-                  if(i.mainGenre == "G.verse.other"){ i.mainGenre = "Verse: Other"; }
-              }
-              else if(i.mainGenre == "G.fiction" ||
-                i.mainGenre == "G.fiction.other"||
-                i.mainGenre == "G.fiction.novel" ||
-                i.mainGenre == "G.fiction.letter"){
-                  i.genre = "Fiction";
-                  if(i.mainGenre == "G.fiction"){ i.mainGenre = "Fiction"; }
-                  if(i.mainGenre == "G.fiction.other"){ i.mainGenre = "Fiction: Other"; }
-                  if(i.mainGenre == "G.fiction.novel"){ i.mainGenre = "Fiction: Novel"; }
-                  if(i.mainGenre == "G.fiction.letter"){ i.mainGenre = "Fiction: Letter"; }
-              }
-              else if(i.mainGenre == ""){
-                i.mainGenre = "Other";
-                i.genre = "Other";
-              }
-          });
-      } else {
-        elem = m;
-      }
-
-      // categorize genres
-      elem.forEach(function(i){
-          if(i.pubDate == "1558-01-23"){
-            i.pubDate = "1558";
-          }
-          if(i.pubDate == "1601-11-30"){
-            i.pubDate = "1601";
-          }
-          if(i.pubDate == "1643-01-28"){
-            i.pubDate = "1643";
-          }
-          if(i.pubDate == "1760-1761"){
-            i.pubDate = "1760";
-          }
-          // i.pubDate = +i.pubDate;
-          i.pubDate = new Date(+i.pubDate,0,1);
-
-            if(i.mainGenre == "G.drama" ||
-              i.mainGenre == "G.drama.prose" ||
-              i.mainGenre == "G.drama.verse" ||
-              i.mainGenre == "G.drama.mixed"){
-                i.genre = "Drama";
-                if(i.mainGenre == "G.drama"){ i.mainGenre = "Drama"; }
-                if(i.mainGenre == "G.drama.prose"){ i.mainGenre = "Drama: Prose"; }
-                if(i.mainGenre == "G.drama.verse"){ i.mainGenre = "Drama: Verse"; }
-                if(i.mainGenre == "G.drama.mixed"){ i.mainGenre = "Drama: Mixed"; }
-            }
-            else if(i.mainGenre == "G.non-fiction" ||
-              i.mainGenre == "G.non-fiction.other" ||
-              i.mainGenre == "G.non-fiction.essay" ||
-              i.mainGenre == "G.non-fiction.letter"){
-                i.genre = "Non-fiction";
-                if(i.mainGenre == "G.non-fiction"){ i.mainGenre = "Non-fiction"; }
-                if(i.mainGenre == "G.non-fiction.other"){ i.mainGenre = "Non-fiction: Other"; }
-                if(i.mainGenre == "G.non-fiction.essay"){ i.mainGenre = "Non-fiction: Essay"; }
-                if(i.mainGenre == "G.non-fiction.letter"){ i.mainGenre = "Non-fiction: Letter"; }
-            }
-            else if(i.mainGenre == "G.verse" ||
-              i.mainGenre == "G.verse.lyric"||
-              i.mainGenre == "G.verse.narrative" ||
-              i.mainGenre == "G.verse.other"){
-                i.genre = "Verse";
-                if(i.mainGenre == "G.verse"){ i.mainGenre = "Verse"; }
-                if(i.mainGenre == "G.verse.lyric"){ i.mainGenre = "Verse: Lyric"; }
-                if(i.mainGenre == "G.verse.narrative"){ i.mainGenre = "Verse: Narrative"; }
-                if(i.mainGenre == "G.verse.other"){ i.mainGenre = "Verse: Other"; }
-            }
-            else if(i.mainGenre == "G.fiction" ||
-              i.mainGenre == "G.fiction.other"||
-              i.mainGenre == "G.fiction.novel" ||
-              i.mainGenre == "G.fiction.letter"){
-                i.genre = "Fiction";
-                if(i.mainGenre == "G.fiction"){ i.mainGenre = "Fiction"; }
-                if(i.mainGenre == "G.fiction.other"){ i.mainGenre = "Fiction: Other"; }
-                if(i.mainGenre == "G.fiction.novel"){ i.mainGenre = "Fiction: Novel"; }
-                if(i.mainGenre == "G.fiction.letter"){ i.mainGenre = "Fiction: Letter"; }
-            }
-            else if(i.mainGenre == ""){
-              i.mainGenre = "Other";
-              i.genre = "Other";
-            }
+      elemDist.sort(function(a,b){
+          return b["value"]-a["value"];
         });
 
-      function dataTransform(meta, elem){
+      // take top 20 distinct in-text element values
+      var elemDistTop = elemDist.slice(0,20);
 
-        // create array of distinct in-text element values
-        var elemDist = d3.nest()
-          .key(function(d){return d.element})
-          .rollup(function(d){return d.length})
-          .entries(elem);
-
-        elemDist.sort(function(a,b){
-            return b["value"]-a["value"];
-          });
-
-        // take top 20 distinct in-text element values
-        var elemDistTop = elemDist.slice(0,20);
-
-        // filter element data to match elements in elemDistTop
-        var elemTop = elem.filter(function(d){
-          var i = 0;
-          elemDistTop.forEach(function(a){
-            if(a.key == d.element){
-              i = 1;
-            }
-          });
-          return i == 1;
-        });
-
-        // elemTop.sort(function(a,b){
-        //   return d3.ascending(a.mainGenre, b.mainGenre);
-        // });
-
-        elemTop.sort(function(a,b){
-          return b["pubDate"]-a["pubDate"];
-        });
-
-        elemTop.sort(function(a,b){
-          return d3.ascending(a.element, b.element);
-        });
-
-        // filter metadata to match texts in elemTop
-        meta.forEach(function(d){
-          d.isTop = 0;
-          elemTop.forEach(function(a){
-            if(a.filename == d.filename){
-              d.isTop = 1;
-            }
-          });
-
-          if (d.isTop != 1) {
-            d.isTop = 0;
-          }
-        });
-
-        var metaTop = meta.filter(function(d){
-          return d.isTop == 1;
-        });
-
-        metaTop.sort(function(a,b){
-          if (a.title < b.title){
-            return -1;
-          }
-          else if (a.title > b.title){
-            return 1;
-          }
-          else{ return 0; }
-        });
-
-        // var metaTop = meta.filter(function(d){
-        //   var i = 0;
-        //   elemTop.forEach(function(a){
-        //     if(a.filename == d.filename){
-        //       i = 1;
-        //     }
-        //   });
-        //   return i == 1;
-        // });
-
-        // create array of distinct genre values
-        var metaTopGenre = d3.nest()
-          .key(function(d){ return d.mainGenre })
-          .rollup(function(d){return d.length})
-          .entries(metaTop);
-
-        metaTopGenre.sort(function(a,b){
-          return d3.ascending(a.key, b.key);
-        })
-
-        // create objects connecting positions of element and genre text to link data
-        var elemDistObj = {};
-        var metaObj = {};
+      // filter element data to match elements in elemDistTop
+      var elemTop = elem.filter(function(d){
+        var i = 0;
         elemDistTop.forEach(function(a){
-          elemDistObj[a.key] = a
+          if(a.key == d.element){
+            i = 1;
+          }
         });
+        return i == 1;
+      });
 
-        metaTopGenre.forEach(function(a){
-          metaObj[a.key] = a
-        });
-
-        elemTop.forEach(function(i){
-          i.sourceData = metaObj[i.mainGenre]
-          i.targetData = elemDistObj[i.element]
-        });
-
-        return {
-          meta: meta,
-          metaTop: metaTop,
-          metaTopGenre: metaTopGenre,
-          elemTop: elemTop,
-          elemDistTop: elemDistTop
-        }
-      };
-
-      async function waitForDataTransform(){
-        var allData = await dataTransform(meta, elem);
-        dispatch.call("dataLoaded", null, allData);
-      };
-
-      waitForDataTransform();
-
-      // dataTransform(meta, elem).then( function(allData){
-      //   //do something with result
-      //   console.log(allData);
-      //   dispatch.call("dataLoaded", null, allData);
+      // elemTop.sort(function(a,b){
+      //   return d3.ascending(a.mainGenre, b.mainGenre);
       // });
 
-      // dispatch.call(Transformation(meta, elem))
+      elemTop.sort(function(a,b){
+        return b["pubDate"]-a["pubDate"];
+      });
 
+      elemTop.sort(function(a,b){
+        return d3.ascending(a.element, b.element);
+      });
 
-      // dispatch.call("dataLoaded", null, meta, metaTop, metaTopGenre, elemDistTop, elemTop);
+      // filter metadata to match texts in elemTop
+      meta.forEach(function(d){
+        d.isTop = 0;
+        elemTop.forEach(function(a){
+          if(a.filename == d.filename){
+            d.isTop = 1;
+          }
+        });
 
+        if (d.isTop != 1) {
+          d.isTop = 0;
+        }
+      });
+
+      var metaTop = meta.filter(function(d){
+        return d.isTop == 1;
+      });
+
+      metaTop.sort(function(a,b){
+        if (a.title < b.title){
+          return -1;
+        }
+        else if (a.title > b.title){
+          return 1;
+        }
+        else{ return 0; }
+      });
+
+      // var metaTop = meta.filter(function(d){
+      //   var i = 0;
+      //   elemTop.forEach(function(a){
+      //     if(a.filename == d.filename){
+      //       i = 1;
+      //     }
+      //   });
+      //   return i == 1;
+      // });
+
+      // create array of distinct genre values
+      var metaTopGenre = d3.nest()
+        .key(function(d){ return d.mainGenre })
+        .rollup(function(d){return d.length})
+        .entries(metaTop);
+
+      metaTopGenre.sort(function(a,b){
+        return d3.ascending(a.key, b.key);
+      })
+
+      // create objects connecting positions of element and genre text to link data
+      var elemDistObj = {};
+      var metaObj = {};
+      elemDistTop.forEach(function(a){
+        elemDistObj[a.key] = a
+      });
+
+      metaTopGenre.forEach(function(a){
+        metaObj[a.key] = a
+      });
+
+      elemTop.forEach(function(i){
+        i.sourceData = metaObj[i.mainGenre]
+        i.targetData = elemDistObj[i.element]
+      });
+
+      return {
+        meta: meta,
+        metaTop: metaTop,
+        metaTopGenre: metaTopGenre,
+        elemTop: elemTop,
+        elemDistTop: elemDistTop
+      }
+    };
+
+    /*async function waitForDataTransform(){
+      var allData = await dataTransform(meta['genres'], meta['gestures']);
+      dispatch.call("dataLoaded", null, allData);
+    };
+
+    waitForDataTransform();*/
   });
+  
 };
--- network.js
+++ network.js
@@ -23,11 +23,6 @@ var curve = d3.line()
   .y(function(d){ return d.y})
   .curve(d3.curveBundle.beta(0.85));
 
-// var colorDrama = "#EC407A",
-//   colorFiction = "#BA68C8",
-//   colorNonFiction = "#03A9F4",
-//   colorVerse = "#00E676";
-
 var colorDrama = "#C2185B",
   colorFiction = "#673AB7",
   colorNonFiction = "#00ACC1",
@@ -71,314 +66,54 @@ function makePath(data){
 
 // dispatch.on("dataLoaded.network",function(meta, metaTop, metaTopGenre, elemDistTop, elemTop){
 dispatch.on("dataLoaded.network",function(allData){
-  var metaTop = allData.metaTop,
-  metaTopGenre = allData.metaTopGenre,
-  elemTop = allData.elemTop,
-  elemDistTop = allData.elemDistTop;
-
-  // create text for elements
-  var ypos = 0;
-  var elemTopLength = elemTop.length;
-
-  svgC.selectAll(".text-elem").remove();
-  svgC.selectAll(".rect-elem").remove();
-
-  var textElem = svgC.selectAll(".text-elem")
-    .data(elemDistTop)
-    .enter()
-    .append("g")
-    .attr("class","text-elem")
-    .append("text")
-    .attr("x", function(d){
-      d.x = col1;
-      return d.x - 4;
-    })
-    .attr("y",function(d){
-      d.y = ypos;
-      d.prop = d.value / elemTopLength;
-      position = ypos + 4 + (d.prop*networkHeight)/2;
-      ypos += ((d.value / elemTopLength)*networkHeight) + 2;
-      return position;
-    })
-    .text(function(d){return d.key})
-    .attr("font-family","sans-serif")
-    .attr("font-size","8px")
-    .attr("text-anchor","end")
-    // .attr("fill","white")
-    .attr("fill","#292826");
-
-  var rectElem = svgC.selectAll(".rect-elem")
-    .data(elemDistTop)
-    .enter()
-    .append("rect")
-    .attr("class","rect-elem")
-    .attr("x",function(d){
-      return d.x;
-    })
-    .attr("y",function(d){
-      return d.y;
-    })
-    .attr("width","1px")
-    .attr("height", function(d){
-      return (d.prop*networkHeight);
-    })
-    // .attr("fill","white")
-    .attr("fill","#292826");
-
-  // create text for genres
-  var textMeta = svgC.selectAll(".text-meta")
-    .data(metaTopGenre);
-
-  textMeta.exit().remove();
-
-  textMetaEnter = textMeta.enter()
-    .append("g")
-    .append("text")
-    .attr("class","text-meta");
-
-  textMeta = textMeta.merge(textMetaEnter)
-    .attr("x", function(d){
-      d.x = col2;
-      return d.x;
-    })
-    .attr("y",function(d,i){
-      d.y = (i+1)*(networkHeight/metaTopGenre.length);
-      return d.y+3;
-    })
-    .text(function(d){return d.key})
-    .attr("font-family","sans-serif")
-    .attr("font-size","8px")
-    .attr("text-anchor","start")
-    .attr("fill", function(d){return scaleColor2(d.key)});
-
-  // create links connecting element text and genre text
-  // console.log(elemTop.filter(d => !d.sourceData));
-  // elemTop = makePath(elemTop);
-
-  svgC.selectAll(".links").remove();
-
-  var link = svgC.selectAll(".links")
-    .data(makePath(elemTop.filter(d => d.sourceData)))
-    .enter()
-    .append("g")
-    .attr("class","links")
-    .datum(function(d){ return d.path; })
-    .append("path")
-    .attr("class","path-links")
-    // .transition()
-    // .duration(500)
-    .attr("d", curve)
-    .style("stroke-width", 1)
-    .style("stroke", function(d){return scaleColor2(d[0].mainGenre); })
-    .style("fill","none")
-    .style("opacity",0.1);
-
-  // interactions
-  textElem
-    .on("mouseover",function(d){
-      clicked = 0;
-      var i = 0;
-      dispatch.call("highlightelem",this,d,i);
-    })
-    .on("mouseout", function(d){
-      if(clicked == 0){
-        dispatch.call("unhighlight");
-      }
-    })
-    .on("click",function(d){
-      clicked = 1;
-      var i = 0;
-      dispatch.call("highlightelem",this,d,i);
-    });
-
-   textMeta
-     .on("mouseover", function(d){
-       clicked = 0;
-       var i = 0;
-       dispatch.call("highlightmeta",this,d,i);
-     })
-     .on("mouseout", function(d){
-       if(clicked == 0){
-         dispatch.call("unhighlight");
-       }
-     })
-     .on("click", function(d){
-       clicked = 1;
-       var i = 0;
-       // d3.select(this)
-       //   .transition()
-       //   .duration(200)
-       //   .style("font-weight","bolder");
-      dispatch.call("highlightmeta",this,d,i);
-      // dispatch.call("filterlistmeta",this,d);
-     });
-
-});
-
-dispatch.on("highlightelem", function(d){
-
-  var metaSet = new Set();
-
-  //highlight element
-  svgC.selectAll(".text-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a.key == d.key){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-   svgC.selectAll(".rect-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a.key == d.key){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-  //highlight links
-  svgC.selectAll(".path-links")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a[0].element == d.key){
-        metaSet.add(a[0].mainGenre);
-        return .15;
-      }
-      else { return 0; }
-    });
-
-  //highlight meta genre
-  svgC.selectAll(".text-meta")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (metaSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-})
-
-dispatch.on("highlightmeta", function(d){
-
-  var elemSet = new Set();
-
-  //highlight meta genre
-  svgC.selectAll(".text-meta")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a.key == d.key){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-  //highlight links
-  svgC.selectAll(".path-links")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a[0].mainGenre == d.key){
-        elemSet.add(a[0].element);
-        return .15;
-      }
-      else { return 0; }
-    });
-
-  //highlight element
-  svgC.selectAll(".text-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (elemSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-  svgC.selectAll(".rect-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (elemSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-});
-
-dispatch.on("highlight.network",function(d){
-  var elemSet = new Set();
-  var metaSet = new Set();
-
-  //highlight links
-  svgC.selectAll(".path-links")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (a[0].filename == d.filename){
-        elemSet.add(a[0].element);
-        metaSet.add(a[0].mainGenre);
-        return .15;
-      }
-      else { return 0; }
-    });
-
-  //highlight element
-  svgC.selectAll(".text-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (elemSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-  svgC.selectAll(".rect-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (elemSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-  //highlight meta genre
-  svgC.selectAll(".text-meta")
-    .transition()
-    .duration(200)
-    .style("opacity",function(a){
-      if (metaSet.has(a.key)){
-        return 1;
-      }
-      else { return .1; }
-    });
-
-});
-
-dispatch.on("unhighlight.network",function(){
-  svgC.selectAll(".text-meta")
-    .transition()
-    .duration(200)
-    .style("opacity",1);
-  svgC.selectAll(".text-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",1);
-  svgC.selectAll(".rect-elem")
-    .transition()
-    .duration(200)
-    .style("opacity",1);
-  svgC.selectAll(".path-links")
-    .transition()
-    .duration(200)
-    .style("opacity",.1);
+  var genres = allData.genres,
+      gestures = allData.gestures,
+      genreList = [];
+  for (var key of genres.keys()) {
+    genreList.push({'key': key, 'value': genres.get(key)});
+  }
+  // Create labels for genres.
+  var genreLabels,
+      totalGestures = 0,
+      ypos = 0,
+      countGestures = function(map) {
+        var numGestures = 0;
+        map.forEach( function(val) {
+          numGestures += val.length;
+        });
+        return numGestures;
+      },
+      genreAxis = svgC.selectAll('.label-genre')
+        .append('g')
+          .attr('class', 'axis')
+          .data(genreList)
+          .enter();
+  // Add labels for each genre.
+  genreLabels = genreAxis.append('text')
+      .attr('class', 'label-plot')
+      .attr("fill", "#292826")
+      .text( function(d) { 
+        console.log(d);
+        totalGestures += countGestures(d.value);
+        return d.key;
+      });
+  // Position the genre labels along their axis. Use a 4px gap in between ranges.
+  var gapPx = 4,
+      hCFree = hC - ( genreList.length * gapPx );
+  genreLabels
+      .attr("x", function() { return col1 - 4; })
+      .attr("y", function(d, i) {
+        var numGestures = countGestures(d.value),
+            // Place labels halfway within the datum's range.
+            yPx = ( (numGestures / 2) * hCFree) / totalGestures,
+            position = ypos + yPx + gapPx;
+        // Use the full range of this datum to determine where the next should start.
+        ypos += (yPx * 2) + (gapPx / 2);
+        //console.log(position);
+        return position;
+      });
+  
+  // Create border rectangles for genre?
+  // Create labels for @type.
+  // 
 });
